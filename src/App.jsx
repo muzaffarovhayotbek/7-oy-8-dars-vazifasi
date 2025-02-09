82f5a10f550cad0b914c5ec545d894e26a83c47b
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-02-09 18:01:55 +0500

    add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Chart from './components/Chart';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero/>
+      <Chart/>
+    </div>
+  );
+}
+
+export default App;
