b244b756aba510762f553de3de4e84c30b86b9ad
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-02-12 23:18:33 +0500

    add charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-
 import Chart from './components/Chart';
+import SemiChart from './components/SemiChart';
+import SemiTwoChart from './components/SemiTwoChart';
+import SemiThreeChart from './components/SemiThree';
+import SemiFour from './components/SemiFour';
+import SemiFiveChart from './components/SemiFive';
+import SemiSixChart from './components/SemiSix';
 function App() {
   return (
     <div>
@@ -18,9 +23,44 @@ function App() {
           </h2>
           <h2 className="w-[1050px] bg-[#DEE2E6] h-[4px]"></h2>
         </div>
-        <Chart/>
-      
-    
+        <div className="flex items-center gap-12 mb-12 w-[198px]">
+          <div className="w-[198px]">
+            <SemiChart />
+            <p className="text-[16px] text-[#495057] font-[400] pt-8">
+              Конституциявий-хукукий саводхонлик
+            </p>
+          </div>
+          <div className="w-[198px] flex flex-col pt-8">
+            <SemiTwoChart />
+            <p className="text-[16px] text-[#495057] font-[400]">
+              Ахборот-коммуникация технологиялари
+            </p>
+          </div>
+          <div className="w-[198px] pt-8">
+            <SemiThreeChart />
+            <p className="text-[16px] text-[#495057] font-[400]">
+              Турли сохадаги ислохотлардан хабардорлик
+            </p>
+          </div>
+        </div>
+        <div className="flex  items-center  gap-12">
+          <div className="w-[198px]">
+            <SemiFour />
+            <p className="text-[16px] text-[#495057]">Узбек тили ва адабиёти</p>
+          </div>
+          <div className="w-[198px]">
+            <p className="text-[16px] text-[#495057]">
+            <SemiFiveChart/>
+              Узбекистон тарихи ва маданияти
+            </p>
+          </div>
+          <div className="w-[198px]">
+            <p className="text-[16px] text-[#495057]">
+              <SemiSixChart/>
+              Аклий салохияти ва мантикий фикрлаш
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
